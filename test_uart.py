6f7a0ea59a55921d9f89a2d4f6f1cdaa86edd3e0
gmonkman <gmonkman@github.com>
2026-09-23 11:11:20 +0100

    init

---
+++ test_uart.py
@@ -0,0 +1,25 @@
+python3 - <<'PY'
+import serial
+import time
+
+s = serial.Serial(
+    '/dev/ttyAMA0',
+    57600,
+    bytesize=8,
+    parity='N',
+    stopbits=1,
+    timeout=1
+)
+
+print("Sending test...")
+s.write(b'HELLO\r\n')
+s.flush()
+
+time.sleep(0.1)
+
+data = s.read(100)
+print("Received:", data.hex())
+print("As text:", repr(data))
+
+s.close()
+PY
